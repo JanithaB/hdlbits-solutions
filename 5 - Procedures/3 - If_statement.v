808fff936bd12f729bc133e8f1fd5a2db3113796
Janitha Rathnayaka <141403556+JanithaB@users.noreply.github.com>
2024-07-30 12:14:21 +0530

    Update 3 - If_statement.v

--- 5 - Procedures/3 - If_statement.v
+++ 5 - Procedures/3 - If_statement.v
@@ -7,7 +7,7 @@ module top_module(
     output wire out_assign,
     output reg out_always   ); 
 	
-    assign out_assign = ((sel_b1 & sel_b2) == 1) ? b : a;
+	assign out_assign = ((sel_b1 & sel_b2) == 1'b1) ? b : a;
     always @(*) begin
         if ((sel_b1 & sel_b2) == 1) begin
             out_always = b;
