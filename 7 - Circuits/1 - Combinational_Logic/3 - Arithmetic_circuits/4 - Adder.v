774ff9b9baec51863fca22e3a4a4fe25b3456fa8
Janitha Rathnayaka <141403556+JanithaB@users.noreply.github.com>
2024-08-01 21:16:53 +0530

    Update 4 - Adder.v

--- 7 - Circuits/1 - Combinational_Logic/3 - Arithmetic_circuits/4 - Adder.v
+++ 7 - Circuits/1 - Combinational_Logic/3 - Arithmetic_circuits/4 - Adder.v
@@ -1,17 +1,15 @@
 module top_module (
-    input [3:0] x,
-    input [3:0] y, 
-    output [4:0] sum);
+	input [3:0] x,
+	input [3:0] y,
+	output [4:0] sum
+);
 
-    wire w1, w2, w3;
-    fa fa0(x[0], y[0], 1'b0, sum[0], w1);
-    fa fa1(x[1], y[1], w1, sum[1], w2);
-    fa fa2(x[2], y[2], w2, sum[2], w3);
-    fa fa3(x[3], y[3], w3, sum[3], sum[4]);
-    
-endmodule
+	// This circuit is a 4-bit ripple-carry adder with carry-out.
+	assign sum = x+y;	// Verilog addition automatically produces the carry-out bit.
 
-module fa (input a, input b, input cin, output sum, output cout);
-    assign sum = a ^ b ^ cin;
-    assign cout = (a&b) | (a&cin) | (b&cin);
+	// Verilog quirk: Even though the value of (x+y) includes the carry-out, (x+y) is still considered to be a 4-bit number (The max width of the two operands).
+	// This is correct:
+	// assign sum = (x+y);
+	// But this is incorrect:
+	// assign sum = {x+y};	// Concatenation operator: This discards the carry-out
 endmodule
